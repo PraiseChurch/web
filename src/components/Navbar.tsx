618d6c458a7dd02d836b481ee82109e8ba8d6d62
johnamielyasis <johnamielyasis@gmail.com>
2023-09-11 20:27:57 -0700

    chore: set max screen size and hamburger conditional render depending on screen size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 // components/Navbar.js
@@ -7,29 +7,38 @@ import Image from "next/image";
 
 export const Navbar = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  const toggleMenu = () => setMobileNavOpen(!mobileNavOpen);
 
   return (
-    <nav className="bg-black p-4">
-      <div className="container mx-auto flex justify-between items-center top-0">
-        <Link href="/" className="text-white font-semibold text-lg">
-          <Image
-            src="/tulip-logo-white.png"
-            alt="praise church west covina logo"
-            className="opacity-1"
-            width={24}
-            height={32}
-            priority
-          />
-        </Link>
-        <div className="hidden: md:flex space-x-4">
-          <Link href="/about" className="text-white hover: text-grey-300">
-            About
+    <nav className="bg-black py-3 px-4">
+      <div className="container mx-auto flex justify-between items-center top-0 max-w-screen-lg">
+          <Link href="/" className="text-white font-semibold text-lg">
+            <Image
+              src="/tulip-logo-white.png"
+              alt="praise church west covina logo"
+              className="opacity-1"
+              width={24}
+              height={32}
+              priority
+            />
           </Link>
-          <Link href="/contact" className="text-white hover: text-grey-300">
-            Contact
-          </Link>
+          <div className="md:hidden flex space-x-4">
+            {!mobileNavOpen ? (
+              <button>
+                <RxHamburgerMenu
+                  color="white"
+                  size="32"
+                  margin="auto 0px"
+                  onClick={() => {
+                    toggleMenu();
+                  }}
+                />
+              </button>
+            ) : (
+              <div className="">asdf</div>
+            )}
+          </div>
         </div>
-      </div>
     </nav>
   );
 };
