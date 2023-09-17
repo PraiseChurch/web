6b9cc85cc4e85bcaf2ab67a17b8a199c28b8cb45
johnamielyasis <johnamielyasis@gmail.com>
2023-09-17 01:05:35 -0700

    feat: initialize footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Navbar } from "./modules";
+import { Footer, Navbar } from "./modules";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <Navbar />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   );
 }
--- src/app/modules/Footer.tsx
+++ src/app/modules/Footer.tsx
@@ -1,3 +1,22 @@
-export default function Footer() {
-  return <h1>hwfooter</h1>;
-}
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white p-4">
+      <div className="container mx-auto flex justify-between">
+        <div className="w-1/3">
+          <h2 className="text-lg font-semibold">Column 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="w-1/3">
+          <h2 className="text-lg font-semibold">Column 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="w-1/3">
+          <h2 className="text-lg font-semibold">Column 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
